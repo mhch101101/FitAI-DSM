72e81a1cd1e1a464b7203e513f960742a1c18b93
Bryan Ochoa <bryan.ochoa@unmsm.edu.pe>
2026-05-29 11:02:29 -0500

    Agrega inyeccion de dependencias

--- app/src/main/java/app/dsm/fitai/di/AppComponent.kt
+++ app/src/main/java/app/dsm/fitai/di/AppComponent.kt
@@ -2,6 +2,7 @@ package app.dsm.fitai.di
 
 import android.app.Application
 import app.dsm.fitai.domain.repository.AuthRepository
+import app.dsm.fitai.domain.repository.RoutineRepository
 import app.dsm.fitai.domain.repository.UserRepository
 import dagger.BindsInstance
 import dagger.Component
@@ -19,6 +20,8 @@ interface AppComponent {
 
     fun userRepository(): UserRepository
     fun authRepository(): AuthRepository
+    fun routineRepository(): RoutineRepository
+
     @Component.Factory
     interface Factory {
         fun create(
--- app/src/main/java/app/dsm/fitai/di/RepositoryModule.kt
+++ app/src/main/java/app/dsm/fitai/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package app.dsm.fitai.di
 
 import app.dsm.fitai.data.repository.AuthRepositoryImpl
+import app.dsm.fitai.data.repository.RoutineRepositoryImpl
 import app.dsm.fitai.data.repository.UserRepositoryImpl
 import app.dsm.fitai.domain.repository.AuthRepository
+import app.dsm.fitai.domain.repository.RoutineRepository
 import app.dsm.fitai.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
@@ -19,5 +21,10 @@ abstract class RepositoryModule {
     abstract fun bindAuthRepository(
         impl: AuthRepositoryImpl
     ): AuthRepository
+
+    @Binds
+    abstract fun bindRoutineRepository(
+        impl: RoutineRepositoryImpl
+    ): RoutineRepository
 
 }
